e813085509fc602889f69e34476c3545b3630e55
akshay-1231 <akshayprajapati424206@gmail.com>
2024-10-01 23:49:40 +0530

    Update BankCall.java

--- BankCall.java
+++ BankCall.java
@@ -2,21 +2,21 @@ package JavaAssignment5;
 
 public class BankCall {
     public static void main(String[] args) {
-        BankA B = new BankA(1000);
-        BankB B1 = new BankB(1500);
-        BankC B2 = new BankC(2000);
+        BankA L = new BankA(1000);
+        BankB M = new BankB(1500);
+        BankC N = new BankC(2000);
 
-        System.out.println("For Bank 1 ");
-        System.out.println("Balance is : "+ B.getBalance());
-        System.out.println("After appying interest: "+B.applyInterest());
+        System.out.println("For Bank A ");
+        System.out.println("Balance is : "+ L.getBalance());
+        System.out.println("After applying interest: "+L.applyInterest());
         System.out.println("==============================================");
-        System.out.println("For Bank 2 ");
-        System.out.println("Balance is : "+ B1.getBalance());
-        System.out.println("After appying interest: "+B1.applyInterest());
+        System.out.println("For Bank B ");
+        System.out.println("Balance is : "+ M.getBalance());
+        System.out.println("After applying interest: "+M.applyInterest());
         System.out.println("==============================================");
-        System.out.println("For Bank 3 ");
-        System.out.println("Balance is : "+ B2.getBalance());
-        System.out.println("After appying interest: "+B2.applyInterest());
+        System.out.println("For Bank C ");
+        System.out.println("Balance is : "+ N.getBalance());
+        System.out.println("After appyling interest: "+N.applyInterest());
         System.out.println("==============================================");
     }
 }
