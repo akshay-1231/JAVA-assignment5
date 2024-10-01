6278265cb3c87ed3327b2025e6f9763a13d954f9
akshay-1231 <akshayprajapati424206@gmail.com>
2024-10-01 23:51:19 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,7 +1,7 @@
 /*.    Create a class to print the area of a square and a rectangle.The class has two methods ‘printArea’ and ‘printPerimeter’ with the same name but different number of parameters. The method for printing area of
 Rectangle has two parameters which are length and breadth respectively while the other method for printing area of square has one parameter which is side of square
 
-*/
+*/// Q2.
 public class Shape {
     public void printArea(double side) {
         double area = side * side;
